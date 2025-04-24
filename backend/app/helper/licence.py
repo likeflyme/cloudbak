34a5364b3802f4765f9c0348ac772ae9d30a2747
likeflyme <likeflyme@gmail.com>
2025-04-24 11:21:32 +0800

    删除加密案例代码

--- backend/app/helper/licence.py
+++ backend/app/helper/licence.py
@@ -77,33 +77,3 @@ class LicenseManager:
         except Exception:
             raise ValueError("Invalid license code")
 
-
-# 示例
-if __name__ == "__main__":
-    version = "v01"  # 加密版本
-    secret_key = "license.cloudbak.org"  # 密钥
-    license_manager = LicenseManager(version, secret_key)
-
-    # client_id = "A7E6DA66-19C5-11F0-B628-462664C98C0C"  # 授权码
-    license_type = LicenseType.PRO  # 授权类型，1为PRO授权
-    # expiry_date = "0"  # 过期时间，0为无期限
-    # order_id = "lost"  # 订单号
-    #
-    # license_info = License(license_type=license_type, client_id=client_id, expiry_date=expiry_date, order_id=order_id)
-    #
-    # print(f"机器码：{client_id}")
-    # print(f"授权类型：{license_type}")
-    # print(f"有效期：{expiry_date}")
-    # print(f"订单号：{order_id}")
-
-    # license_code = license_manager.generate_license(license_info)
-    license_code = 'v01o4FmNconninXxhU4wusStTODdt0e3g9WCCaxaJa8hEKjAh5e/9VXtoan4RCAc7+6irW5Sul1QJXhmk20xGj5SQiIHwpuNV6RvfVzxS2ycWhcf74tmYtUeNboshyzVPraKxWPPUa1tzLJzYwT+I/+Vt7QWp6ecitQxLoIkJG2hBud/1zHNvEXbe2cplq+H5yvJ3GbWzPF8GZFm9FifSZupw=='
-    print("生成的授权码:", license_code)
-
-    # 解析授权码
-    parsed_data = license_manager.parse_license(license_code)
-    print("解析后的数据:", parsed_data)
-
-    # license_code = "v01HcRfRffeu18KzwxPf2cBs6RU1dDhhOd+UZ+BqnzfdwNwnTUB+IJuHC8OuzxXRHi0YlxdYAgPEVQMaS7YaPIIq8wQ6kO7vVRBiAu+QxYN1/nI22SFHAmLFEG9HjRg4/r4/SMruYIK+s0uqu8+QC9P9ldt9lrYhQNc0JyXd4oiglgTOaTtvCxDfH3kGWZDv7rx"
-    # parsed_data = license_manager.parse_license(license_code)
-    # print("解析后的数据:", parsed_data)
